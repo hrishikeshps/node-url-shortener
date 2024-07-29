a26f804b06fff0192fa6703531c245e2cbb2635a
Hrishikesh <hrishikeshps@gmail.com>
2024-07-30 00:28:11 +0530

    update syntax to esm and package.json

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express');
-
+import express from 'express'
+import { nanoid } from 'nanoid'
 const app = express();
 
+// const router = express.Router();
+
+app.get('/', function(req, res){
+    const id = nanoid(8);
+    console.log(`id is -->> `, id);
+})
 
 app.listen(3001, () => console.log('Server Running at 3001'))
