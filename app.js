53a5ac9513f4f4d44e61bf63f725cc3bc0a924cf
Hrishikesh <hrishikeshps@gmail.com>
2024-07-30 23:50:36 +0530

    changes app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ import express from 'express'
 import { nanoid } from 'nanoid'
 const app = express();
 
-// const router = express.Router();
+const router = express.Router();
 
-app.get('/', function(req, res){
+router.get('/', function(req, res){
     const id = nanoid(8);
     console.log(`id is -->> `, id);
-})
+});
+
 
 app.listen(3001, () => console.log('Server Running at 3001'))
