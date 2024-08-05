b8931c5e27fae8ce6514894b7a5b010d4a425c2e
Hrishikesh <hrishikeshps@gmail.com>
2024-08-05 12:19:35 +0530

    add get short url fxn

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,28 @@
-console.log('Test says hello world!!!');
+console.log('Test says hello world!!!')
+
+function createURL(){
+    const long_url = document.getElementById("long_url").value
+
+    if(long_url == ''){
+        alert('Please provide a url!')
+        return
+    }
+
+    const apiUrl = '/get-short-url'
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ long_url })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('data is here ==> ', data)
+    })
+    .catch((err) => {
+        console.error('Error: ', err)
+    })
+
+}
